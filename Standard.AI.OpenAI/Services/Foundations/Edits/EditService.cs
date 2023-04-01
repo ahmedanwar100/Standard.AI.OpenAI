e5fec8b4542e88323eacdf547cb1ab2036f8e88a
Ricardo Cruz <rcruzfreelance@outlook.com>
2023-04-01 18:31:05 -0400

    FOUNDATIONS: Created edit service

---
+++ Standard.AI.OpenAI/Services/Foundations/Edits/EditService.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standard.AI.OpenAI.Models.Services.Foundations.Edits;
+
+namespace Standard.AI.OpenAI.Services.Foundations.Edits
+{
+    internal class EditService : IEditService
+    {
+        public ValueTask<Edit> EditAsync(Edit edit)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Standard.AI.OpenAI/Services/Foundations/Edits/IEditService.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standard.AI.OpenAI.Models.Services.Foundations.Edits;
+
+namespace Standard.AI.OpenAI.Services.Foundations.Edits
+{
+    internal interface IEditService
+    {
+        ValueTask<Edit> EditAsync(Edit edit);
+    }
+}
